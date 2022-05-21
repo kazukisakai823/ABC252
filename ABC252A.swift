6085e250b3c784b11b0c45be21ee338fc7ae66b9
kazukisakai <101170572+kazukisakai823@users.noreply.github.com>
2022-05-21 22:11:38 +0900

    ABC252A問題の解答
    解答例

---
+++ ABC252A.swift
@@ -0,0 +1,4 @@
+var n = Int(readLine()!)!
+n = n - 97
+var ans = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
+print(ans[n])
